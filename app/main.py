33673787e61d3a6f429c70b44a55119e71b17e55
Ammar Sajjid <ammarsajjidali@gmail.com>
2026-05-10 08:51:04 +0400

    logic to remove null fields from repsonse

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ def health_check() -> dict:
     return {"status": "ok"}
 
 
-@app.post("/v1/analyze/sentiment", response_model=SentimentResponse)
+@app.post("/v1/analyze/sentiment", response_model=SentimentResponse, response_model_exclude_none=True)
 def analyze_sentiment(request: SentimentRequest, include_debug: bool = False) -> SentimentResponse:
     result = sentiment_service.predict(request.text)
 
